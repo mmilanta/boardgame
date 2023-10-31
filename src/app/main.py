477c6e7b74914549c2fa2ffb938d559183b3f58c
mmilanta <marco.milanta@gmail.com>
2023-10-31 22:45:25 +0100

    fix: add exeption handling

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
 
 from src.game_logic.actions import Action, get_game, take_action
 from src.game_logic.board import Board
 from src.game_logic.game import Game
 from src.game_logic.player import Player
+from src.game_logic.exceptions import IllegalActionException
 
 app = FastAPI()
 
@@ -21,3 +24,13 @@ def get_game_endpoint(game_id: int) -> Game:
 def post_action_endpoint(action: Action) -> Game:
     game = take_action(action=action)
     return game
+
+
+@app.exception_handler(IllegalActionException)
+def unsupported_language_exception_handler(
+    request: Request, exc: IllegalActionException
+):
+    return JSONResponse(
+        status_code=400,
+        content={"message": f"IllegalActionException: {str(exc)}"},
+    )
